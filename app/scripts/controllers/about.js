8d0de70de2a3083d91729fea0195b6c5e914fdc7
rafael carlucci <icculrac@rafaels-MacBook-Air.local>
2015-08-12 00:29:08 -0430

    Prototipo de Carousel, estructura generada con Yeoman y generador Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carouselApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the carouselApp
+ */
+angular.module('carouselApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
