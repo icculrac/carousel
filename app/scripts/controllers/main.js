ba9f2dc9777acd129e06f6cdf77d8a5bbb0b1874
rafael carlucci <icculrac@rafaels-MacBook-Air.local>
2015-08-12 08:54:53 -0430

    Carousel Funcional con 3 imagenes en la capeta images y un ancho de imagen del 100% y menu con mas ventanas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the carouselApp
  */
 angular.module('carouselApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.setInterval=5000;
+    
+    $scope.slides = [
+    {
+    	title:'Frozen',
+    	image:'../../images/capi.jpeg',
+    	text:'Prueba el nuevo cereal'
+    },
+    {
+    	title:'Los Increibles',
+    	image:'../../images/capitan.jpg',
+    	text:'Prueba el nuevo cereal'
+    },
+    {
+    	title:'The Avengers',
+    	image:'../../images/jobs.jpg',
+    	text:'Prueba el nuevo cereal'
+  }
     ];
   });
