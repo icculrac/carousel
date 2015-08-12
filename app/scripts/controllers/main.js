0ec4d6520bb2fa589bcb3f3f502371d813664cdc
rafael carlucci <icculrac@rafaels-MacBook-Air.local>
2015-08-12 09:31:45 -0430

    Agregada la seccion del footer en la pagina principal y agregada la seccion de las redes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,23 +9,77 @@
  */
 angular.module('carouselApp')
   .controller('MainCtrl', function ($scope) {
+  	var url='../../images/';
     $scope.setInterval=5000;
     
     $scope.slides = [
-    {
-    	title:'Frozen',
-    	image:'../../images/capi.jpeg',
-    	text:'Prueba el nuevo cereal'
-    },
-    {
-    	title:'Los Increibles',
-    	image:'../../images/capitan.jpg',
-    	text:'Prueba el nuevo cereal'
-    },
-    {
-    	title:'The Avengers',
-    	image:'../../images/jobs.jpg',
-    	text:'Prueba el nuevo cereal'
-  }
+	    {
+	    	title:'Frozen',
+	    	image:url+'capi.jpeg',
+	    	text:'Prueba el nuevo cereal'
+	    },
+	    {
+	    	title:'Los Increibles',
+	    	image:url+'capitan.jpg',
+	    	text:'Prueba el nuevo cereal'
+	    },
+	    {
+	    	title:'The Avengers',
+	    	image:url+'jobs.jpg',
+	    	text:'Prueba el nuevo cereal'
+	  	}
+    ];
+
+    $scope.footer=[
+	    {
+	    	title:'Nosotros',
+	    	text:'xoxoxoxoxox',
+	    	text2:'xoxoxoxoxox',
+	    	text3:'xoxoxoxoxox',
+	    	text4:'xoxoxoxoxox',
+	    	text5:'xoxoxoxoxox',
+	    	text6:'xoxoxoxoxox',
+	    	text7:'xoxoxoxoxox'
+	    },
+	    {
+	    	title:'Productos',
+	    	text:'xoxoxoxoxox',
+	    	text2:'xoxoxoxoxox',
+	    	text3:'xoxoxoxoxox',
+	    	text4:'xoxoxoxoxox',
+	    	text5:'xoxoxoxoxox',
+	    	text6:'xoxoxoxoxox',
+	    	text7:'xoxoxoxoxox'
+	    },
+	    {
+	    	text:'xoxoxoxoxox',
+	    	text2:'xoxoxoxoxox',
+	    	text3:'xoxoxoxoxox',
+	    	text4:'xoxoxoxoxox',
+	    	text5:'xoxoxoxoxox',
+	    	text6:'xoxoxoxoxox',
+	    	text7:'xoxoxoxoxox'
+	    },
+	    {
+	    	title:'Certificados',
+	    	text:'xoxoxoxoxox',
+	    	text2:'xoxoxoxoxox',
+	    	text3:'xoxoxoxoxox',
+	    	text4:'xoxoxoxoxox'
+	    },
+	    {
+	    	title:'Promociones',
+	    	text:'xoxoxoxoxox'
+	    },
+	    {
+	    	title:'Contactos',
+	    	text:'xoxoxoxoxox',
+	    	text2:'xoxoxoxoxox',
+	    	text3:'xoxoxoxoxox',
+	    	text4:'xoxoxoxoxox',
+	    	text5:'xoxoxoxoxox',
+	    	text6:'xoxoxoxoxox',
+	    	text7:'xoxoxoxoxox'
+	    }
     ];
   });
